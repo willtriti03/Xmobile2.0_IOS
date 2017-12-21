433247a6e139d99d1d2ad070f471eb2ef3176410
will <willtriti03@naver.com>
2017-12-21 13:38:14 +0900

    set

---
+++ XMobile_VER2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMobile_VER2.0
+//
+//  Created by Will on 2017. 12. 20..
+//  Copyright © 2017년 Will. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
